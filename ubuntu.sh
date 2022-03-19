af19308cfce9ffbc8086e90a6dc1759e211c7d8d
alexandros <alexandroskapretsos@gmail.com>
2022-03-19 23:26:26 +0200

    ubuntu.sh: Added sdl packages

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,9 +3,13 @@
 sudo dpkg --add-architecture i386
 sudo apt update && sudo apt install -y \
 wine wine64 wine32 \
+libsdl2-2.0-0 libsdl2-2.0-0:i386 \
+libsdl2-image-2.0-0 \
+libsdl2-mixer-2.0-0 \
+libsdl2-ttf-2.0-0 \
+libsdl2-net-2.0-0 \
 libvulkan1 libvulkan1:i386 \
 libasound2-plugins:i386 \
-libsdl2-2.0-0:i386 \
 libdbus-1-3:i386 \
 libsqlite3-0:i386
 
