a1a0ff476f88aae8f74806e462f433a6c9129e45
alexandros <alexandroskapretsos@gmail.com>
2022-03-20 00:20:45 +0200

    debian.sh: Forgot to add -y.

--- debian.sh
+++ debian.sh
@@ -15,4 +15,4 @@ libsqlite3-0:i386
 
 echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_11/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
 wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_11/Release.key -O- | sudo apt-key add -
-sudo apt install lutris
+sudo apt install -y lutris
