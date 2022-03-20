444da6fd6e6cffb67ef9e7869a26c14f27202db9
alexandros <alexandroskapretsos@gmail.com>
2022-03-20 16:07:51 +0200

    added download scirpt for libretm games

---
+++ games.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+
+path="$HOME/Games"
+url_sauerbraten='https://sourceforge.net/projects/sauerbraten/files/sauerbraten/2020_11_29/sauerbraten_2020_12_29_linux.tar.bz2/download'
+url_warsow='https://warsow.net/warsow-2.1.2.tar.gz'
+
+download_tar() {
+	curl -L "$1" > "$2"; tar -xf "$2" -C "$path"; rm "$2"
+}
+
+mkdir -p "$path"
+download_tar "$url_sauerbraten" 'sauerbraten.tar.bz2'
+download_tar "$url_warsow" 'warsow-2.1.2.tar.gz'
